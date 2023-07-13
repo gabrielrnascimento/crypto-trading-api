7248b58fc3ba56332024caef0cfda63d2dec88b5
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-07-13 12:15:11 -0300

    test: ensure DbAddOffer calls AddAccountRepository with correct values

---
+++ src/data/usecases/add-offer/db-add-offer.spec.ts
@@ -0,0 +1,55 @@
+import { type User, type Coin, type Wallet } from '../../../domain/models';
+import { type AddOfferModel } from '../../../domain/usecases/add-offer';
+import { type AddOfferRepository } from '../../protocols/add-offer-repository';
+import { DbAddOffer } from './db-add-offer';
+
+class AddOfferRepositoryStub implements AddOfferRepository {
+  async add (data: AddOfferModel): Promise<AddOfferModel> {
+    return makeAddOfferModel();
+  }
+}
+
+const makeCoinMock = (): Coin => ({
+  id: 1,
+  code: 'ANY',
+  unitPrice: 3.50
+});
+
+const makeUserMock = (): User => ({
+  id: 1,
+  email: 'any_email@mail.com',
+  wallets: [
+
+  ]
+});
+
+const makeWalletMock = (): Wallet => ({
+  id: 1,
+  coins: [
+    makeCoinMock()
+  ],
+  name: 'any_wallet',
+  owner: makeUserMock()
+});
+
+const makeAddOfferModel = (): AddOfferModel => ({
+  coin: makeCoinMock(),
+  createdBy: makeUserMock(),
+  wallet: makeWalletMock(),
+  quantity: 10,
+  unitPrice: 3.50,
+  totalPrice: 35.0
+});
+
+describe('DbAddOffer', () => {
+  test('should call AddAccountRepository with correct values', async () => {
+    const addOfferRepositoryStub = new AddOfferRepositoryStub();
+    const sut = new DbAddOffer(addOfferRepositoryStub);
+    const addSpy = jest.spyOn(addOfferRepositoryStub, 'add');
+
+    const offer = makeAddOfferModel();
+    await sut.add(offer);
+
+    expect(addSpy).toHaveBeenCalledWith(offer);
+  });
+});
