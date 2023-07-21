87fd508ef0818b11aed22b5564246430323952a2
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-07-20 22:49:30 -0300

    chore: update jest coverage config

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,10 @@ module.exports = {
     roots: ['<rootDir>/src/'],
     collectCoverageFrom: [
       '<rootDir>/src/**/*.ts',
+      '!<rootDir>/src/**/index.ts',
+      '!<rootDir>/src/main/**',
+      '!<rootDir>/src/infra/database/typeorm/config/**',
+      '!<rootDir>/src/infra/database/typeorm/test/**',
     ],
     coverageDirectory: 'coverage',
     testEnvironment: 'jest-environment-node',
